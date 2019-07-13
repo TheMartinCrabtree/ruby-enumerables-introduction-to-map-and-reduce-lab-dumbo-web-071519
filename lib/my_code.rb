4a0cb7d77e57bee2909d8f356fac89b602d03d78
Martin Crabtree <martin.crabtree@gmail.com>
2019-07-13 17:01:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,9 +48,11 @@ def reduce_to_total(source_array, starting_point)
   while counter < source_array.length
     total+=source_array[counter]
     counter+=1 
-  end
+  #end
 
-  return total
+  #return total
+  
+  
 end
 
 
